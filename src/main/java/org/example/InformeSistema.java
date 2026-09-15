37973aa213e982c9a9af8176fb9cac4e10c05712
Isaac Rodríguez <dam26@a09eq17>
2026-09-15 10:15:13 +0200

    Commit2

--- src/main/java/org/example/InformeSistema.java
+++ src/main/java/org/example/InformeSistema.java
@@ -1,4 +1,12 @@
 package org.example;
-
+import java.lang.Runtime;
 public class InformeSistema {
+    static void main(){
+        Runtime runtine = Runtime.getRuntime();
+
+        int procesadores = runtine.availableProcessors();
+        System.out.println("PROCESADORES");
+        System.out.println("=".repeat(50));
+        System.out.println( "Disponibles JVM: "+ procesadores);
+    }
 }
